ea66e7db14ee3439676e78db3c0a88bc9443ba75
yomirein <brosmulletxtale@example.com>
2025-01-07 00:45:25 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module org.yomirein.sochatjava {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.yomirein.sochatjava to javafx.fxml;
+    exports org.yomirein.sochatjava;
+}
---
+++ src/main/java/org/yomirein/sochatjava/HelloController.java
@@ -0,0 +1,11 @@
+package org.yomirein.sochatjava;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    public void handleSubmitButtonAction(ActionEvent actionEvent) {
+    }
+}
